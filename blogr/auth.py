83ecf41daca217450c5a2fcd4d28889748194d13
Pablo-Echegaray <pabloechegaray95@gmail.com>
2024-04-01 16:32:54 -0300

    Administrar perfiles.

--- blogr/auth.py
+++ blogr/auth.py
@@ -81,6 +81,27 @@ def login_required(view):
         return view(**kwargs)
     return wrapped_view 
 
-@bp.route('/profile')
-def profile():
-    return 'Página de profile'
+@bp.route('/profile/<int:id>', methods = ('GET', 'POST'))
+@login_required
+def profile(id):
+    user = User.query.get_or_404(id)
+    
+    if request.method == 'POST':
+        user.username = request.form.get('username')
+        password  = request.form.get('username')
+        
+        error = None
+        if len(password) != 0:
+            user.password = generate_password_hash(password)
+        elif len(password) > 0 and len(password) < 6:
+            error = 'La contraseña debe tener más de 5 carateres'
+        
+        if error is not None:
+            flash(error)
+        else:
+            db.session.commit()
+            return redirect(url_for('auth.profile', id = user.id))
+        
+        flash(error)
+               
+    return render_template('auth/profile.html', user = user)
